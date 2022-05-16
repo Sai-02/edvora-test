31f9b0c434e5724340d93daf9b67ee7bcf14aecd
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-05-16 12:21:59 +0530

    [update] : User api added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import UpcomingRides from "./components/UpcomingRides/UpcomingRides";
 import { useEffect, useState } from "react";
 import PastRides from "./components/PastRides/PastRides";
 function App() {
+  const [user, setUser] = useState({});
   const [rides, setRides] = useState([]);
   useEffect(() => {
     fetch("https://assessment.api.vweb.app/rides")
@@ -20,11 +21,17 @@ function App() {
       .catch((e) => {
         console.log(e);
       });
+    fetch("https://assessment.api.vweb.app/user")
+      .then((res) => res.json())
+      .then((res) => {
+        setUser(res);
+      })
+      .catch((e) => console.log(e));
   }, []);
   return (
     <Router>
       <div className="container">
-        <Header />
+        <Header user={user} />
         <div className="wrapper">
           <Navbar />
           <Routes>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "../styles/Header.css";
 import profileLogo from "../images/header-profile.svg";
-const Header = () => {
+const Header = ({ user }) => {
   return (
     <header className="header">
       <div className="">
         <h1 className="header-text">Edvora</h1>
       </div>
       <div className="header-profile">
-        <p className="header-user-name">Dhruv Singh</p>
-        <img src={profileLogo} alt="" className="header-profile-img" />
+        <p className="header-user-name">{user.name}</p>
+        <img src={user.url} alt="" className="header-profile-img" />
       </div>
     </header>
   );
